5a32291f08f5f3ab314b23b6bac6f6d73a838c23
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2022-04-29 23:22:45 +0600

    mongodb ready

--- index.js
+++ index.js
@@ -7,6 +7,27 @@ const app=express();
 app.use(cors())
 app.use(express.json())
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@maincluster.znen7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+try{
+    client.connect()
+    const itemCollection = client.db("bookathonWarehouse").collection("items");
+
+    
+
+}
+finally{
+
+}
+}
+
+run().catch(console.dir)
+
 app.get('/', (req, res)=>{
     res.send('Server Is ON')
 })
